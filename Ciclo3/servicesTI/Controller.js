950c1c2d572d3188e9c2b2df3a8a985c094f85da
Vinicius Basseto Mira <vibm_@hotmail.com>
2021-09-06 18:56:49 -0300

    Aula 5
    Aula 5 Concluída

--- Ciclo3/servicesTI/Controller.js
+++ Ciclo3/servicesTI/Controller.js
@@ -65,7 +65,7 @@ app.get('/listaservicos', async(req, res)=>{
 app.get('/ofertas', async(req, res)=>{
     await servico.count('id')
     .then(function(servicos){
-        res.json({servicos});
+        res.json(servicos);
     });    
 });
 
@@ -85,12 +85,82 @@ app.get('/servico/:id', async(req,res)=>{
     });
 });
 
+app.get('/atualizaservico', async(req, res)=>{
+    await servico.findByPk(1)
+    .then(servico =>{
+        servico.nome='HTML/CSS/JS';
+        servico.descricao='Páginas estáticas e dinâmicas estilizadas';
+        servico.save();
+        return res.json({servico})
+    });
+});
+
+// faz o update
+app.put ('/editarservico', async(req, res)=>{
+    servico.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Serviço foi alterado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro na alteração do serviço."
+        });
+    });
+});
+
+// busca pedidos com base em um serviço
+app.get('/servicospedidos', async(req, res)=>{
+    await servico.findByPk(1, {
+        include:[{all:true}]
+    }).then(servico =>{
+        return res.json({servico});
+})
+});
+
+//atualização - editar o pedido
+app.put('/editarpedido', async (req, res)=>{
+    pedido.update(req.body, {
+        where: {ServicoId: req.body.ServicoId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Pedido modificado com sucesso."
+        });
+    }).catch(function(erro){
+            return res.status(400).json({
+                error: true,
+                message: "Não foi possível modificar o pedido."
+        });
+    });
+});
 
 
+//EXCLUSÃO
+app.get('/excluircliente', async(req, res)=>{
+    await cliente.destroy({
+        where: {id: 2}
+    });
+});
 
-
-
-
+app.delete('/apagarcliente/:id', (req, res)=>{
+    cliente.destroy({
+        where: {id: req.params.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Cliente foi excluido com sucesso"
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error:true,
+            message: "Não foi possível excluir o cliente."
+        });
+    });
+});
 
 
 /*EXERCÍCIOS AULA 4*/
@@ -103,7 +173,6 @@ app.get('/listaclientes', async(req, res)=>{
         res.json({clientes})
     });
 });
-
 // 2. Visualize os clientes em ordem de antiguidade. ---OK
 app.get('/listaclientesold', async(req, res)=>{
     await cliente.findAll({
@@ -112,7 +181,6 @@ app.get('/listaclientesold', async(req, res)=>{
         res.json({clientes})
     });
 });
-
 // 3. Visualize todos os pedidos. ---OK
 app.get('/listapedidos',async(req, res)=>{
     await pedido.findAll({
@@ -121,7 +189,6 @@ app.get('/listapedidos',async(req, res)=>{
         res.json({pedidos})
     });
 });
-
 // 4. Visualize o pedido por ordem a partir do maior valor. ---OK
 app.get('/listapedidosmaiorvalor',async(req, res)=>{
     await pedido.findAll({
@@ -130,8 +197,6 @@ app.get('/listapedidosmaiorvalor',async(req, res)=>{
         res.json({pedidos})
     });
 });
-
-
 // 5. Informe quantos clientes estão na nossa base de dados. ---OK
 app.get('/quantidadeclientes', async(req, res)=>{
     await cliente.count('id')
@@ -139,7 +204,6 @@ app.get('/quantidadeclientes', async(req, res)=>{
         res.json({clientes});
     });    
 });
-
 // 6. Informe a quantidade de pedidos solicitados. ---
 app.get('/quantidadepedidos', async(req, res)=>{
     await pedido.count('id')
@@ -147,7 +211,6 @@ app.get('/quantidadepedidos', async(req, res)=>{
         res.json({pedidos});
     });    
 });
-
 
 /* DESAFIO AULA 4
     Qual é o valor total que o cliente X gastou na ServicesTI?
@@ -159,8 +222,57 @@ app.get('/totalCliente/:id', async(req,res)=>{
     });
 }); 
 
+/**EXERCÍCIOS AULA 5
+ 1. Faça uma busca por serviços de clientes passando o id do cliente no corpo da requisição
 
+ 2. Utilize a rota para consultar clientes e faça a edição de um cliente pelo método 'put'
 
+ 3. Utilize a rota para consultar pedidos e faça a edição de um pedido pelo método 'put'
+ */
+
+// 1. 
+app.get('/servicosclientes', async(req, res)=>{
+    await cliente.findByPk(1, {
+        include:[{all:true}]
+    }).then(cliente =>{
+        return res.json({cliente});
+    })
+});
+
+// 2. 
+app.put('/editarcliente', async (req, res)=>{
+    cliente.update(req.body, {
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Cliente modificado com sucesso."
+        });
+    }).catch(function(erro){
+            return res.status(400).json({
+                error: true,
+                message: "Não foi possível modificar o cliente."
+        });
+    });
+});
+
+// 3. já criado na linha 125
+/*app.put('/editarpedido', async (req, res)=>{
+    pedido.update(req.body, {
+        where: {ServicoId: req.body.ServicoId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Pedido modificado com sucesso."
+        });
+    }).catch(function(erro){
+            return res.status(400).json({
+                error: true,
+                message: "Não foi possível modificar o pedido."
+        });
+    });
+});
+*/
 
 
 
@@ -171,3 +283,14 @@ let port=process.env.PORT || 3000 // var port que recebe do ambiente uma porta O
 app.listen(port,(req,res)=>{
     console.log('Servidor ativo');
 })
+
+
+
+
+
+
+/*
+
+TOTAL DE ROTAS CRIADAS:  22
+
+*/
